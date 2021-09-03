1777308d691c24a3e8564eb3649f2ff0b0d9a474
cccaaannn <can.kurt.aa@gmail.com>
2021-09-04 00:03:27 +0300

    options file added

---
+++ src/config/options.tsx
@@ -0,0 +1,16 @@
+import { ICurrency } from "../types/types";
+
+export const durumLinks: string[] = [
+    "https://www.google.com/search?q=dürümcü",
+    "https://www.google.com/search?q=dürüm",
+    "https://www.google.com/search?q=yemekseeti+dürüm",
+    "https://www.duckduckgo.com?q=dürüm",
+    "https://www.nefisyemektarifleri.com/kategori/tarifler/hizli-yemekler/durum-tarifleri/"
+]
+
+export const currency: ICurrency = { name: "Türk Lirası", shortName: "TL", symbol: "₺" };
+export const durumEmoji: string = "🌯";
+export const defaultDurumCost: number = 15;
+export const maxDurumDrawingNumber: number = 5000;
+export const maxDurumErrorMessage: string = "Burada duralım sayfa kasmasın, biraz da muz ye 🍌";
+export const enjoyMealMessage: string = "Afiyet olsun kardeşim";
